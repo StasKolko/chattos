d9edb9bf44c170e51899f65a74be35bdc731847f
StasDev <b2bstas@gmail.com>
2024-09-13 09:48:10 +0300

    add HtmlWebpackPlugin and html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = (env) => {
   const mode = env.mode || 'development';
   const isDev = mode === 'development';
   const paths = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: path.resolve(__dirname, 'dist'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
   }
 
   return {
@@ -18,5 +21,8 @@ module.exports = (env) => {
       path: paths.output,
       clean: true,
     },
+    plugins: [new HtmlWebpackPlugin({
+      template: paths.html
+    })],
   };
 };
