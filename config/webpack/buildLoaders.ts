e208f47ce1f6eeae6ab40107e90d138f560136f7
StasDev <b2bstas@gmail.com>
2024-09-14 10:07:48 +0300

    add svgr and file loaders

--- config/webpack/buildLoaders.ts
+++ config/webpack/buildLoaders.ts
@@ -2,12 +2,18 @@ import { RuleSetRule } from "webpack";
 
 import { getBabelLoader } from "./loaders/getBabelLoader";
 import { getCssLoader } from "./loaders/getCssLoader";
+import { getFileLoader } from "./loaders/getFileLoader";
+import { getSvgLoader } from "./loaders/getSvgLoader";
 
 export function buildLoaders(isDev: boolean) {
   const babelLoader = getBabelLoader(isDev);
   const cssLoader = getCssLoader(isDev);
+  const fileLoader = getFileLoader();
+  const svgLoader = getSvgLoader();
 
   const loaders: RuleSetRule[] = [
+    svgLoader,
+    fileLoader,
     babelLoader,
     cssLoader
   ]
---
+++ config/webpack/loaders/getFileLoader.ts
@@ -0,0 +1,8 @@
+import { RuleSetRule } from "webpack";
+
+export function getFileLoader(): RuleSetRule {
+  return {
+    test: /\.(png|jp(e*)g|gif|woff|woff2)$/,
+    type: 'asset/resource',
+  };
+}
